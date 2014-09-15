7524858df6845255fee24001eddd4b10d1c8ebfc
Dale Wijnand <dale.wijnand@gmail.com>
2014-09-15 17:45:31 +0100

    Tweak access log msg & log to access logger.

--- app/filters/LoggingFilter.scala
+++ app/filters/LoggingFilter.scala
@@ -1,18 +1,30 @@
 package filters
 
-import play.api.Logger
+import org.joda.time.{DateTime, DateTimeZone}
+import org.joda.time.format.ISODateTimeFormat
+import org.slf4j.{MDC, LoggerFactory}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
 
 object LoggingFilter extends EssentialFilter {
+  val log = LoggerFactory.getLogger("HttpAccessLogger")
+  val dateTimeFmt = ISODateTimeFormat.dateTime()
+
   def apply(nextFilter: EssentialAction) = new EssentialAction {
     def apply(requestHeader: RequestHeader) = {
-      val startTime = System.currentTimeMillis
+      val startTime = DateTime now DateTimeZone.UTC
       nextFilter(requestHeader) map { result =>
-        val endTime = System.currentTimeMillis
-        val requestTime = endTime - startTime
-        Logger info
-          s"${requestHeader.method} ${requestHeader.uri} took ${requestTime}ms and returned ${result.header.status}"
+        val endTime = DateTime now DateTimeZone.UTC
+        val requestTime = endTime.getMillis - startTime.getMillis
+
+        val startTimeStr = startTime.toString(dateTimeFmt)
+        val httpMethodStr = requestHeader.method.padTo(4, " ").mkString
+        val resultStatusCode = result.header.status
+        val requestTimeStr = (requestTime + "ms").padTo(6, " ").mkString
+        val requestUri = requestHeader.uri
+
+        log info s"$startTimeStr  $httpMethodStr $resultStatusCode  $requestTimeStr $requestUri"
+
         result withHeaders "Request-Time" -> requestTime.toString
       }
     }
