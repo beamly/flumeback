bbc4ceb5ea96888460e16432159e478f89f88676
Dale Wijnand <dale.wijnand@gmail.com>
2014-09-15 15:45:42 +0100

    Add additional headers & proxy string.

--- src/main/scala/flumeback/FlumebackAppender.scala
+++ src/main/scala/flumeback/FlumebackAppender.scala
@@ -1,25 +1,46 @@
 package flumeback
 
+import ch.qos.logback.classic.pattern.ThrowableProxyConverter
 import ch.qos.logback.classic.spi.ILoggingEvent
-import ch.qos.logback.core.AppenderBase
+import ch.qos.logback.core.{AppenderBase, CoreConstants}
 import dispatch.Defaults._
 import dispatch._
+import org.json4s.JsonDSL._
+import org.json4s.native.JsonMethods._
 
+import scala.collection.JavaConverters._
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
 class FlumebackAppender extends AppenderBase[ILoggingEvent] {
+  val throwableProxyConverter = new ThrowableProxyConverter
+
+  override def start(): Unit = {
+    throwableProxyConverter.start()
+
+    super.start()
+  }
+
   def append(le: ILoggingEvent): Unit = {
+    val message = le.getFormattedMessage
+    val stackStr = throwableProxyConverter.convert(le)
+    val body = message + CoreConstants.LINE_SEPARATOR + stackStr
+
+    val headers = Map(
+      "timestamp" -> le.getTimeStamp.toString,
+          "level" -> le.getLevel.toString,
+       "threadId" -> le.getThreadName,
+         "source" -> le.getLoggerName
+    ) ++ le.getMDCPropertyMap.asScala.toMap
+    val headersStr = compact(render(headers))
+
     val resp = Http((
       host("localhost", 16002)
       setContentType("application/json", "UTF-8")
     ) << s"""
         |[{
-        |   "headers" : {
-        |     "timestamp" : "${le.getTimeStamp}",
-        |     "host" : "random_host.example.com"
-        |   },
-        |   "body" : "${le.getFormattedMessage}"
+        |   "headers" : $headersStr,
+        |   "body" : "$body"
         |}]
       """.stripMargin
     )
