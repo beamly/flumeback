eba8cababb56b8f9a8650adf635ed249ece4b81a
Dale Wijnand <dale.wijnand@gmail.com>
2014-09-15 09:27:45 +0100

    Add /datetime & scheduled datetime logs.

---
+++ app/Global.scala
@@ -0,0 +1,16 @@
+import org.joda.time.DateTime
+import play.Logger
+import play.api.Play.current
+import play.api.libs.concurrent.Akka
+import play.api.{Application, GlobalSettings}
+import scala.concurrent.duration._
+
+object Global extends GlobalSettings {
+  lazy implicit val actorSystem = Akka.system
+  lazy implicit val execContext = actorSystem.dispatcher
+
+  override def onStart(app: Application): Unit = {
+    Logger info s"Application has started at ${new DateTime()}"
+    actorSystem.scheduler.schedule(1.minute, 1.minute)(Logger info s"Tick: ${new DateTime()}")
+  }
+}
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,15 @@
 package controllers
 
+import org.joda.time.DateTime
 import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def datetime = Action {
+    Ok(new DateTime().toString())
   }
 }
