132eacc9f3fecb541879f912b4daf6e844a1b205
Dale Wijnand <dale.wijnand@gmail.com>
2014-09-15 16:37:15 +0100

    Add configuration points to FlumebackAppender.

--- src/main/scala/flumeback/FlumebackAppender.scala
+++ src/main/scala/flumeback/FlumebackAppender.scala
@@ -8,17 +8,31 @@ import dispatch._
 import org.json4s.JsonDSL._
 import org.json4s.native.JsonMethods._
 
+import scala.beans.BeanProperty
 import scala.collection.JavaConverters._
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.util.control.NonFatal
 
 class FlumebackAppender extends AppenderBase[ILoggingEvent] {
   val throwableProxyConverter = new ThrowableProxyConverter
 
+  @BeanProperty var host = "localhost"
+  @BeanProperty var port = 16311
+  @BeanProperty var timeout = "1 second"
+
+  private var await: Duration = 1.second
+
   override def start(): Unit = {
     throwableProxyConverter.start()
 
-    super.start()
+    try {
+      await = Duration(timeout)
+      super.start()
+    } catch {
+      case NonFatal(e) =>
+        addError(s"Failed to parse timeout: $timeout", e)
+    }
   }
 
   def append(le: ILoggingEvent): Unit = {
@@ -35,7 +49,7 @@ class FlumebackAppender extends AppenderBase[ILoggingEvent] {
     val headersStr = compact(render(headers))
 
     val resp = Http((
-      host("localhost", 16002)
+      dispatch.host(host, port)
       setContentType("application/json", "UTF-8")
     ) << s"""
         |[{
@@ -44,6 +58,7 @@ class FlumebackAppender extends AppenderBase[ILoggingEvent] {
         |}]
       """.stripMargin
     )
-    Await.result(resp, 1.minute)
+
+    Await.result(resp, await)
   }
 }
