dbc5e845a1119108b09289432e252f16e52bdde4
Dale Wijnand <dale.wijnand@gmail.com>
2014-10-28 09:40:50 +0000

    Instead of waiting, capture failures & use logback mechanics.

--- src/main/scala/flumeback/FlumebackAppender.scala
+++ src/main/scala/flumeback/FlumebackAppender.scala
@@ -10,8 +10,6 @@ import org.json4s.native.JsonMethods._
 
 import scala.beans.BeanProperty
 import scala.collection.JavaConverters._
-import scala.concurrent.Await
-import scala.concurrent.duration._
 import scala.util.control.NonFatal
 
 class FlumebackAppender extends AppenderBase[ILoggingEvent] {
@@ -19,21 +17,15 @@ class FlumebackAppender extends AppenderBase[ILoggingEvent] {
 
   @BeanProperty var host = "localhost"
   @BeanProperty var port = 16311
-  @BeanProperty var timeout = "1 second"
 
-  private var await: Duration = 1.second
   private[flumeback] var http: Http = Http
+
+  private val allowedRepeats = 5
+  private var exceptionCount = 0
 
   override def start(): Unit = {
     throwableProxyConverter.start()
-
-    try {
-      await = Duration(timeout)
-      super.start()
-    } catch {
-      case NonFatal(e) =>
-        addError(s"Failed to parse timeout: $timeout", e)
-    }
+    super.start()
   }
 
   override def stop(): Unit = {
@@ -70,7 +62,14 @@ class FlumebackAppender extends AppenderBase[ILoggingEvent] {
     )
 
     val resp = http(req)
-    Await.result(resp, await)
+
+    resp onFailure {
+      case NonFatal(e) =>
+        if (exceptionCount < allowedRepeats)
+          addError("Appender [" + name + "] failed to append.", e)
+        exceptionCount += 1
+    }
+
     ()
   }
 }
